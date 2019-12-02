737f891ae165d69fb82d6d6994a15a115bbd36eb
Clayton Brown <45186932+Whit3XLightning@users.noreply.github.com>
2019-12-02 14:31:25 -0600

    Fixed config values and removed unprofessional comments.

--- client/main.lua
+++ client/main.lua
@@ -1,19 +1,13 @@
---so it fucking works, whoever comes in and desides to change something in here make sure it's not in the config.
---if you have a problem with the code suck my fucking cock
---i would also like to thank my mom for feeding me a meal during this proccess
-
---I desided I'll probably release this on my gh, the code it pretty cool. will also try and work on saving their ped and giving it back to them when they get out!
-
 local jailed = false
 local int = 0
-local time = config.jail.defTime
+local time = config.defTime
 
 RegisterNetEvent('wld-jailer:jail')
 AddEventHandler('wld-jailer:jail', function(input)
     if input ~= nil then time = input end
-    if time > config.jail.maxTime then time = config.jail.maxTime end
+    if time > config.maxTime then time = config.maxTime end
     if not jailed then
-        jail(time) --realizing i'm going to have to do all of this from scratch
+        jail(time)
         jailed = true
     end
 end)
@@ -23,7 +17,7 @@ AddEventHandler('wld-jailer:unjail', function()
     if jailed then jailed = false end
 end)
 
-function jail(time) --why tf did I make this a function?
+function jail(time)
     if DoesEntityExist(GetPlayerPed(-1)) then
         Citizen.CreateThread(function()
             if IsPedSittingInAnyVehicle(GetPlayerPed(-1)) then
@@ -31,57 +25,57 @@ function jail(time) --why tf did I make this a function?
                 SetEntityAsMissionEntity(pv, true, true )
                 DeleteVehicle(pv)
             end
-            SetEntityCoords(GetPlayerPed(-1), config.jail.coords)
-            SetEntityInvincible(GetPlayerPed(-1), true) --idk if I should do this but idrc either way
-            local skin = GetHashKey(config.jail.skin)
+            SetEntityCoords(GetPlayerPed(-1), config.coords)
+            SetEntityInvincible(GetPlayerPed(-1), true)
+            local skin = GetHashKey(config.skin)
             RequestModel(skin)
-	        while not HasModelLoaded(skin) do --fucking slow ass 5m
+	        while not HasModelLoaded(skin) do
                 Citizen.Wait(0)
 	        end
-            SetPlayerModel(PlayerId(), skin) --why tf is it playerid instead of playerpedid
+            SetPlayerModel(PlayerId(), skin)
             while time > 0 and jailed do
                 ResetPed(GetPlayerPed(-1))
-                if IsEntityAwayFromJail(GetPlayerPed(-1), config.jail.radius) then
-                    time = time + config.jail.escapeExtension
+                if IsEntityAwayFromJail(GetPlayerPed(-1), config.radius) then
+                    time = time + config.escapeExtension
                     TriggerEvent('chat:addMessage', {color={255,102,0},muiltiline=true,args={"[JAIL]","Your Jail time has been extended to "..time.." seconds due to a escape attempt!"}})
-                    SetEntityCoords(GetPlayerPed(-1), config.jail.coords) --where i'm starting to hate my life
+                    SetEntityCoords(GetPlayerPed(-1), config.coords)
                 end
                 int = int + 1
-                if int == config.jail.annouceInterval and time ~= 1 then --and a time check because it would say 0 seconds until release, like no bitch, fuck you...
-                    local undertime = time - 1 --this is the most retarted thing about this script
+                if int == config.annouceInterval and time ~= 1 then
+                    local undertime = time - 1
                     TriggerEvent('chat:addMessage', {color={255,102,0},muiltiline=true,args={"[JAIL]",undertime.." more second(s) until release"}})
                     int = 0
                 end
-                Citizen.Wait(1000) --i mean... it still does the job for the count down :|
+                Citizen.Wait(1000)
                 time = time - 1
             end
-            int = 0 --like wtf was I thinking
+            int = 0
             jailed = false
             ResetPed(GetPlayerPed(-1))
             SetEntityInvincible(GetPlayerPed(-1), false)
-            SetEntityCoords(GetPlayerPed(-1), config.jail.releaseCoords)
+            SetEntityCoords(GetPlayerPed(-1), config.releaseCoords)
             TriggerServerEvent('wld-jailer:unjailed')
         end)
     end
 end
 
-function ResetPed(ped) --this is pretty cool might use it again somewhere else
+function ResetPed(ped)
     SetPedArmour(ped, 0)
-    RemoveAllPedWeapons(ped, true) --no rdm at the prison now mother fuckers
+    RemoveAllPedWeapons(ped, true)
     ClearPedBloodDamage(ped)
     ResetPedVisibleDamage(ped)
     ClearPedLastWeaponDamage(ped)
     if IsPedSittingInAnyVehicle(ped) then
         local rpv = GetVehiclePedIsIn(ped, false)
         ClearPedTasksImmediately(ped)
-        SetVehicleDoorsLocked(rpv, 2) --Incase the vehicle doesn't delete bc 1s is shit (2 means people can get out but not in)
-        SetEntityAsMissionEntity(rpv, true, true)
-        DeleteVehicle(rpv) --this works just fine idk why tf people use the raw native
-        TriggerEvent('chat:addMessage', {color={255,102,0},muiltiline=true,args={"[JAIL]","You can't drive a car in jail, dumb dumb..."}}) --hehe
+        SetVehicleDoorsLocked(rpv, 2)
+SetEntityAsMissionEntity(rpv, true, true)
+        DeleteVehicle(rpv)
+        TriggerEvent('chat:addMessage', {color={255,102,0},muiltiline=true,args={"[JAIL]","You can't drive a car in jail, dumb dumb..."}})
     end
 end
 
 function IsEntityAwayFromJail(e, md)
     local d = Vdist(config.jail.coords, GetEntityCoords(e))
-    if d > md then return true else return false end --really hope this works
+    if d > md then return true else return false end
 end
