8cf273825feff9ef72359cea9bd83a73fed772cb
Clayton Brown <45186932+Whit3XLightning@users.noreply.github.com>
2019-12-14 15:43:55 -0600

    Updated load order of client scripts.

--- __resource.lua
+++ __resource.lua
@@ -1,7 +1,7 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
 client_scripts {
-    "client/main.lua",
-    "config.lua"
+    "config.lua",
+    "client/main.lua"
 }
 server_script "server/main.lua"
